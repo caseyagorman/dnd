1ea8a6b1abb6871b9b141364afe251416025dd12
Casey Gorman <caseyagorman@gmail.com>
2019-02-24 13:35:04 -0800

    first commit

---
+++ src/Target.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { DropTarget } from "react-dnd";
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    hovered: monitor.isOver(),
+    item: monitor.getItem()
+  };
+}
+
+class Target extends Component {
+  render() {
+    const { connectDropTarget, hovered, item } = this.props;
+    return connectDropTarget(<div className="target">Target</div>);
+  }
+}
+export default DropTarget("item", {}, collect)(Target);
